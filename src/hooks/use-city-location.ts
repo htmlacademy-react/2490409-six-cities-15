b3a1f99bd5ce2f39e37206eb99d5ee702b672024
Pavel Volkov <wolkow937@gmail.com>
2024-03-06 15:53:58 +0200

    fixed warning from useEffect in useCityLocation

--- src/hooks/use-city-location.ts
+++ src/hooks/use-city-location.ts
@@ -3,15 +3,17 @@ import { LatLng, Map } from 'leaflet';
 import { CityType, LocationType } from '../mocks';
 
 function useCityLocation(map: Map | null, city: CityType, location: LocationType) {
+  const { latitude, longitude, zoom } = location;
+
   useEffect(() => {
     if (!map) {
       return;
     }
 
     map
-      .panTo(new LatLng(location.latitude, location.longitude))
-      .setZoom(location.zoom);
-  }, [city]);
+      .panTo(new LatLng(latitude, longitude))
+      .setZoom(zoom);
+  }, [city, latitude, longitude, zoom, map]);
 }
 
 export default useCityLocation;
