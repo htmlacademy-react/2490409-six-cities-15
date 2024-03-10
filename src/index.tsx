1bfaa53c14a36a8f54e330335a3ae85268c6625e
Pavel Volkov <wolkow937@gmail.com>
2024-03-11 00:21:57 +0200

    used data from redux store for main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
-import { offers } from './mocks';
 import {store} from './store';
 import {Provider} from 'react-redux';
 
@@ -12,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App offers={offers} />
+      <App/>
     </Provider>
   </React.StrictMode>
 );
