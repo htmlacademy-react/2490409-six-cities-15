350950acab949acefe37f94aecff54995ca090ad
Pavel Volkov <wolkow937@gmail.com>
2024-03-17 02:41:09 +0200

    connected login and -out logic to interface

--- src/components/atoms/header/sign-out-button/sign-out-button.tsx
+++ src/components/atoms/header/sign-out-button/sign-out-button.tsx
@@ -1,10 +1,19 @@
-import { ReactElement } from 'react';
+import { MouseEventHandler, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
+import { store } from '../../../../store';
+import { logoutAction } from '../../../../store/slices/user/thunk.ts';
+import { getToken } from '../../../../services';
 
-function SignOutButton({link}: {link: string}): ReactElement {
+function SignOutButton(): ReactElement {
+  const handleOnClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
+    event.preventDefault();
+
+    store.dispatch(logoutAction(getToken()));
+  };
+
   return (
     <li className="header__nav-item">
-      <Link className="header__nav-link" to={link}>
+      <Link className="header__nav-link" to="#" onClick={handleOnClick}>
         <span className="header__signout">Sign out</span>
       </Link>
     </li>
--- src/store/slices/user/extra-reducers.ts
+++ src/store/slices/user/extra-reducers.ts
@@ -1,16 +1,16 @@
-import {AUTH_STATUS} from '../../../constants';
-import {UserStateType} from './index.ts';
-import {PayloadAction} from '@reduxjs/toolkit';
-import {AuthenticatedUserType} from '../../../types';
+import { AUTH_STATUS } from '../../../constants';
+import { UserStateType } from './index.ts';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { AuthenticatedUserType } from '../../../types';
 
 const setUser = (state: UserStateType, action: PayloadAction<AuthenticatedUserType>) => {
   state.user = action.payload;
-  state.authStatus = AUTH_STATUS.Auth;
+  state.authorizationStatus = AUTH_STATUS.Auth;
 };
 
 const clearUser = (state: UserStateType) => {
   state.user = null;
-  state.authStatus = AUTH_STATUS.NoAuth;
+  state.authorizationStatus = AUTH_STATUS.NoAuth;
 };
 
 export {
