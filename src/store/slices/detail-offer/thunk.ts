66902b561d4514dbd47ec42927ed243a2c961bef
Pavel Volkov <wolkow937@gmail.com>
2024-03-25 02:01:58 +0200

    used extra argument instead of importing API in thunks

--- src/store/slices/detail-offer/thunk.ts
+++ src/store/slices/detail-offer/thunk.ts
@@ -1,13 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { CommentData, OfferData, OfferDetailData } from '../../../types';
 import { AsyncActionsArgsType, setOfferIdInRoute } from '../../helpers.ts';
-import { API } from '../../../services';
 import { API_ROUTE } from '../../../constants';
 
 const fetchDetailOfferAction = createAsyncThunk<OfferDetailData, OfferData['id'], AsyncActionsArgsType>(
   'fetch/detailOffer',
-  async (id) => {
-    const response = await API.get<OfferDetailData>(setOfferIdInRoute(API_ROUTE.Get.DetailOffer, id));
+  async (id, { extra: api}) => {
+    const response = await api.get<OfferDetailData>(
+      setOfferIdInRoute(API_ROUTE.Get.DetailOffer, id)
+    );
 
     return response.data;
   },
@@ -15,8 +16,10 @@ const fetchDetailOfferAction = createAsyncThunk<OfferDetailData, OfferData['id']
 
 const fetchNearbyOffersAction = createAsyncThunk<OfferData[], OfferData['id'], AsyncActionsArgsType>(
   'fetch/nearByOffers',
-  async (id) => {
-    const response = await API.get<OfferData[]>(setOfferIdInRoute(API_ROUTE.Get.NearBy, id));
+  async (id, { extra: api}) => {
+    const response = await api.get<OfferData[]>(
+      setOfferIdInRoute(API_ROUTE.Get.NearBy, id)
+    );
 
     return response.data;
   },
@@ -24,8 +27,10 @@ const fetchNearbyOffersAction = createAsyncThunk<OfferData[], OfferData['id'], A
 
 const fetchCommentsAction = createAsyncThunk<CommentData[], OfferData['id'], AsyncActionsArgsType>(
   'fetch/fetchComments',
-  async (id) => {
-    const response = await API.get<CommentData[]>(setOfferIdInRoute(API_ROUTE.Get.Comments, id));
+  async (id, { extra: api}) => {
+    const response = await api.get<CommentData[]>(
+      setOfferIdInRoute(API_ROUTE.Get.Comments, id)
+    );
 
     return response.data;
   },
@@ -37,8 +42,10 @@ const addCommentAction = createAsyncThunk<
   AsyncActionsArgsType
 >(
   'send/addComment',
-  async ({id: offerId, comment, rating}) => {
-    const response = await API.post<CommentData>(setOfferIdInRoute(API_ROUTE.Post.AddComment, offerId), { comment, rating });
+  async ({id: offerId, comment, rating}, { extra: api}) => {
+    const response = await api.post<CommentData>(
+      setOfferIdInRoute(API_ROUTE.Post.AddComment, offerId), { comment, rating }
+    );
 
     return response.data;
   },
