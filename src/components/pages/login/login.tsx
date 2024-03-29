47cc563b43b206aee433b5a409ef47a8d3b6a213
Pavel Volkov <wolkow937@gmail.com>
2024-03-29 17:50:00 +0200

    button gets a random city value on login page

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -4,15 +4,19 @@ import { LoginForm } from '../../molecules';
 import { TabButton } from '../../atoms';
 import { CITIES } from '../../../constants';
 import { createMainRouteWithCity } from '../../../utils';
+import { CitiesType } from '../../../types';
 
 function LoginPage(): ReactElement {
+  const randomNumber = Math.round(Math.random() * 6);
+  const randomCityKey = Object.keys(CITIES)[randomNumber] as CitiesType;
+
   return (
     <ContentOnTheRight
-      leftSideContent={<LoginForm />}
+      leftSideContent={<LoginForm/>}
       rightSideButton={
         <TabButton
-          tabName={CITIES.Amsterdam} isSelected={false}
-          link={createMainRouteWithCity(CITIES.Amsterdam)}
+          tabName={CITIES[randomCityKey]} isSelected={false}
+          link={createMainRouteWithCity(CITIES[randomCityKey])}
         />
       }
     />
