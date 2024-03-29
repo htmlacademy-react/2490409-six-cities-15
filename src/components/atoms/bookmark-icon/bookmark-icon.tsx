2ba4e6aea1a3bed964c0c81976f1d32cc2167151
Pavel Volkov <wolkow937@gmail.com>
2024-03-29 21:04:15 +0200

    Revert "added redirect to login page for bookmark-icon.tsx if user is not logged in"
    This reverts commit a776e5e74fb98b32f57fd944f516dac41daafef0.

--- src/components/atoms/bookmark-icon/bookmark-icon.tsx
+++ src/components/atoms/bookmark-icon/bookmark-icon.tsx
@@ -3,9 +3,6 @@ import classNames from 'classnames';
 import { MouseEventHandler, useCallback } from 'react';
 import { changeFavoriteStatusAction } from '../../../store/slices/offers/thunk.ts';
 import { useAppDispatch } from '../../../store/helpers.ts';
-import { useAuthStatus } from '../../../hooks';
-import { APP_ROUTE, AUTH_STATUS } from '../../../constants';
-import { useNavigate } from 'react-router-dom';
 
 type BookmarkIconProps = {
   id: OfferData['id'];
@@ -18,17 +15,9 @@ type BookmarkIconProps = {
 };
 function BookmarkIcon({id, isActive = false, type = 'place-card', size}: BookmarkIconProps) {
   const dispatch = useAppDispatch();
-  const authStatus = useAuthStatus();
-  const navigate = useNavigate();
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
     e.preventDefault();
-
-    if (authStatus !== AUTH_STATUS.Auth) {
-      navigate(APP_ROUTE.Login);
-
-      return;
-    }
 
     dispatch(
       changeFavoriteStatusAction(
