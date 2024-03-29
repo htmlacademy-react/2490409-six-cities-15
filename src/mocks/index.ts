2cef06e59b2d57c4beeb3eb70024031b9ce5b66c
Pavel Volkov <wolkow937@gmail.com>
2024-03-29 21:04:19 +0200

    Revert "deleted mocks"
    This reverts commit 1358f98b29e8210b95409273c57f0c90ea9abcf2.

---
+++ src/mocks/index.ts
@@ -0,0 +1,10 @@
+import { comments } from './comments.ts';
+import { detailOffer } from './detail.ts';
+import { offers, getOffersInNear } from './offers.ts';
+
+export {
+  comments,
+  detailOffer,
+  offers,
+  getOffersInNear,
+};
