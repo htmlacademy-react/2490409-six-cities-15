e24841de7733e399e6af2201b426c5f0859c1a17
Pavel Volkov <wolkow937@gmail.com>
2024-04-06 03:17:47 +0200

    refactored selectors.test.ts

--- src/store/slices/offers/selectors.test.ts
+++ src/store/slices/offers/selectors.test.ts
@@ -1,28 +1,22 @@
 import { offersSelectors } from './selectors.ts';
-import { OffersStateType } from './index.ts';
-import { REQUEST_STATUS } from '../../../constants';
-import { makeFakeOffer, makeOfferAdditionalData } from '../../../utils/tests';
+import { makeFakeOffersState } from '../../../utils/tests';
 
 
 describe('offers\' selectors', () => {
-
-  const state: OffersStateType = {
-    offers: Array(5).map((_item, index) => makeFakeOffer(String(index))),
-    additionalOfferData: makeOfferAdditionalData('1'),
-    detailOfferReviews: null,
-    nearbyOffersIds: ['2'],
-    offersRequestStatus: REQUEST_STATUS.Idle,
-    reviewRequestStatus: REQUEST_STATUS.Idle,
-    activeOfferId: null,
-  };
-
   it('should select offers from state', () => {
+    const state = makeFakeOffersState({
+      offersLen: 5,
+    });
     const offers = offersSelectors.offers(state);
 
-    expect(offers[0]).toEqual(state.offers[0]);
+    expect(offers).toEqual(state.offers);
   });
 
   it('should select additional offer data from state', () => {
+    const state = makeFakeOffersState({
+      offersLen: 5,
+      additionalDataIndex: 0,
+    });
     const additionalOfferData = offersSelectors.additionalOfferData(state);
 
     expect(additionalOfferData).toEqual(state.additionalOfferData);
