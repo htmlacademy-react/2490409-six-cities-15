939a89c02bc3ba138c6387e84164bc0d1ba8d9b3
Pavel Volkov <wolkow937@gmail.com>
2024-04-07 18:34:33 +0200

    added sort-selector-item.test.tsx

---
+++ src/components/atoms/sort-selector-item/sort-selector-item.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import SortSelectorItem from './sort-selector-item.tsx';
+import {SortTypesType} from '../../../types';
+
+describe('Sort selector item component', () => {
+  it('should be in document, have content from props, to be tag "li", to have class "places__option--active" if is selected', () => {
+    const id = 'sort-selector-item';
+    const content: SortTypesType = 'Price: high to low';
+
+    render(<SortSelectorItem value={content} onClick={() => {}} isSelected/>);
+
+    const selectorItem = screen.getByTestId(id);
+
+    expect(selectorItem).toBeInTheDocument();
+    expect(selectorItem).toHaveTextContent(content);
+    expect(selectorItem.tagName.toLowerCase()).toBe('li');
+    expect(selectorItem).toHaveClass('places__option--active');
+  });
+});
